dbe8e9b3b3e7e1bb650b8a87ab59e72dfeeef337
Steven Dang <Steven.D.Dang@gmail.com>
2019-09-12 21:57:53 -0500

    Fixing repo

---
+++ app/src/main/java/com/examples/diceroller/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.examples.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener{
+            rollDice(diceImage1)
+            rollDice(diceImage2)
+        }
+
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+    }
+
+    private fun rollDice(dice: ImageView) {
+
+        var randomInt: Int = Random().nextInt(6) + 1
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        dice.setImageResource(drawableResource)
+
+    }
+}
